7987646dfab8d8173c382a14943babb0927ac0d7
Daniil <daniil.saraikin@yandex.ru>
2020-06-25 19:24:27 +0300

    check

--- src/main/java/ru/newsfeed/trendsoft/TrendsoftApplication.java
+++ src/main/java/ru/newsfeed/trendsoft/TrendsoftApplication.java
@@ -1,16 +1,13 @@
 package ru.newsfeed.trendsoft;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TrendsoftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrendsoftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrendsoftApplication.class, args);
+    }
 
 }
